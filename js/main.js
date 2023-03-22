c3086e71905657467706ec53f3b7f4cb9e0e8952
MAbdurahman <mdbdrrhm@gmail.com>
2023-03-22 09:11:31 -0500

    Add preloader.svg and favicon.ico

---
+++ js/main.js
@@ -0,0 +1,18 @@
+/* ============================================
+            preloader
+===============================================*/
+$(window).on('load', function () {
+  // makes sure that whole site is loaded
+  $('#preloader-gif, #preloader').fadeOut(5000, function () {});
+});
+
+/*=============================================
+          js-voice-note-app
+================================================*/
+$(window).onload = function () {
+
+
+
+
+
+}; //end of window onload anonymous function
